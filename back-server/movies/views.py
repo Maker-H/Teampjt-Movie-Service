d22c7721c11e2906120fde20564e45394d7a2eb2
rlvy98 <rlvy98@naver.com>
2023-05-17 15:29:17 +0900

    feat: Model, Serializers  생성
    영화 정보 조회에 필요한 model인 Movie와 평론 조회 및 작성에 필요한
    Comment를 생성하였습니다.
    
    SMS-2

---
+++ back-server/movies/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404, get_list_or_404
+from .serializers import MovieListSerializer, MovieSerializer
+from .models import Movie
+
+# Create your views here.
+
