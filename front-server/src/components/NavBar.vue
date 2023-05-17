3f3dcbddca8a1f899c2facc51faa4524b7bee951
Heesom <heesom.hs@gmail.com>
2023-05-17 16:03:31 +0900

    feat: 라우터 모음을 App에서 NavBar로 분리
    - SMS-6 에서 완성한 nav bar를 NavBar라는 컴포넌트로 분리
    
    SMS-16

---
+++ front-server/src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav>
+      <router-link :to="{name: 'HomeView'}">Home</router-link> |
+      <router-link :to="{name: 'CommentView'}">Comment</router-link> |
+      <router-link :to="{name: 'RecommandView'}">Recommand</router-link> |
+      <router-link :to="{name: 'SearchView'}">Search</router-link> |
+      <router-link :to="{name: 'ProfileView'}">Profile</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<style>
+
+</style>
