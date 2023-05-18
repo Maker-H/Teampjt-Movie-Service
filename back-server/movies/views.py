da0d62b677d9fc59271b3df3a4b2b7ceec99258a
Heesom <heesom.hs@gmail.com>
2023-05-18 17:33:30 +0900

    feat: 좋아요 기능 추가
    - Movie 모델에 좋아요 N:M 필드 추가
    - 좋아요/좋아요 취소 메소드 추가
    - 좋아요 한 영화 반환하는 메소드 추가
    
    SMS-34

--- back-server/movies/views.py
+++ back-server/movies/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import get_object_or_404, get_list_or_404
 from .serializers import MovieListSerializer, MovieSerializer, CommentSerializer, GenreSerializer
 from .models import Movie, Comment, Genre
 
-# Create your views here.
 @api_view(['GET'])
 def movie_list(request):
     if request.method == 'GET':
@@ -59,14 +58,32 @@ def comment_create(request, movie_pk):
             serializer.save(movie=movie)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
 @api_view(['GET'])
 def genre_list(request):
     if request.method == 'GET':
         genres = get_list_or_404(Genre)
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-        
 
+
+@api_view(['POST'])
+def create_likes(request, movie_pk):
+    if request.method == 'POST':
+        movie = get_object_or_404(Movie, pk=movie_pk)
+        if movie.like_users.filter(pk=request.user.pk).exists():
+            movie.like_users.remove(request.user)
+            return Response({'message': 'Movie unliked successfully.'}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            movie.like_users.add(request.user)
+            return Response({'message': 'Movie liked successfully.'}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def liked_list(request):
+    user = request.user
+    liked_movies = user.like_movies.all()
+    return Response(liked_movies, status=status.HTTP_200_OK)
 
 
 
