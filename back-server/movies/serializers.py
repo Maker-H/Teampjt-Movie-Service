58cf886d83b2ea469ea1353d191f25341d30f260
Heesom <heesom.hs@gmail.com>
2023-05-19 10:06:16 +0900

    feat: 로그인 권한 처리와 토큰 방식 수정
    - 로그인을 해야하는 api에 대해서 로그인을 해야 사용할 수 있게 수정
    - jwt방식에 문제가 있는걸 발견해서 웹토큰이 아닌 일반 토큰을 사용하게
      수정
    
     SMS-63

--- back-server/movies/serializers.py
+++ back-server/movies/serializers.py
@@ -6,13 +6,15 @@ class MovieListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
-    
+
+
 class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
         fields = '__all__'
         read_only_fields = ('movie',)
+
 
 class MovieSerializer(serializers.ModelSerializer):
     comment_set = CommentSerializer(many=True, read_only=True)
@@ -21,7 +23,8 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
-    
+
+
 class GenreSerializer(serializers.ModelSerializer):
 
     class Meta:
