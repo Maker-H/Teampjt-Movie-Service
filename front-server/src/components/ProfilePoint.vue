c8b705088f5647173ccaa5f11176b2a9f66758ea
rlvy98 <rlvy98@naver.com>
2023-05-23 15:45:40 +0900

    feat: profile css 추가
    profile page에 대한 css를 추가했습니다. 카카오페이를 사용하는 포인트
    파트와 유저가 좋아요한 목록을 받아볼 수 있는 영화 즐겨찾기 파트의 css를
    완료했습니다.
    
    SMS-84

--- front-server/src/components/ProfilePoint.vue
+++ front-server/src/components/ProfilePoint.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <span>현재 포인트: </span>
+  <div class="text-white">
+    <span>현재 포인트</span>
     <h3>{{ userPoint }}P</h3>
     <form @submit.prevent="getPayment">
-      <label for="amount">amount</label>
-      <select name="amounts" id="amount" v-model="amountValue">
-        <option value='1000'>1,000원</option>
-        <option value='5000'>5,000원</option>
-        <option value='10000'>10,000원</option>
-      </select>
-      <input type="submit" value="결제" />
+      <div class="d-flex justify-content-center align-items-center">
+        <label for="amount" style="margin-right:20px"><b>포인트 충전</b></label>
+        <div style="max-width: 200px;">
+          <select class="form-select" name="amounts" id="amount" aria-label="select example" v-model="amountValue">
+            <option selected>금액 선택</option>
+            <option value='1000'>1,000원</option>
+            <option value='5000'>5,000원</option>
+            <option value='10000'>10,000원</option>
+          </select>
+        </div>
+        <div>
+          <input class="image-input" type="submit" value=""/>
+        </div>
+      </div>
     </form>
   </div>
 </template>
@@ -42,6 +49,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .image-input {
+    background-image: url('../../public/images/payment_icon_yellow_large.png');
+    margin-top: 10px;
+    margin-left: 10px;
+    padding: 20px 40px;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    color: transparent;
+    text-shadow: 0 0 0 black;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
 </style>
